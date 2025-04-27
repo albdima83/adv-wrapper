04dc32e065f8eb513d971414f294caeeb3afbb9e
Albdima <alberto.dimauro@mediaset.it>
2025-04-27 19:10:53 +0200

    chore: create a simple logger

--- packages/lib/src/custom/utils/logger.ts
+++ packages/lib/src/custom/utils/logger.ts
@@ -1,52 +1,75 @@
-import pino from "pino";
+export type LogLevel = "fatal" | "error" | "warn" | "info" | "debug" | "trace";
 
-function getLogConfigFromParams(): { enable: boolean; level: pino.Level } {
+const LOG_LEVEL_ORDERS = ["debug", "info", "warn", "error", "fatal"];
+
+const MAP_CONSOLE_FN: Record<LogLevel, typeof console.log> = {
+  debug: console.debug || console.log,
+  info: console.info || console.log,
+  warn: console.warn || console.log,
+  error: console.error || console.log,
+  fatal: console.error || console.log,
+};
+
+class BasicLogger {
+  private enable: boolean;
+  private level: LogLevel;
+
+  constructor(config: { enable?: boolean; level?: LogLevel }) {
+    this.enable = config.enable || false;
+    this.level = config.level || "info";
+  }
+
+  private log(level: LogLevel, tag: string, message: string, ...args: any[]) {
+    if (!this.enable) {
+      return;
+    }
+    if (
+      LOG_LEVEL_ORDERS.indexOf(level) >= LOG_LEVEL_ORDERS.indexOf(this.level)
+    ) {
+      const timestamp = new Date().toISOString();
+      const fn = MAP_CONSOLE_FN[level] ?? console.log;
+      fn(`[${tag}][${level}]`, message, ...args);
+    }
+  }
+
+  debug(tag: string, message: string, ...args: any[]) {
+    this.log("debug", tag, message, ...args);
+  }
+
+  info(tag: string, message: string, ...args: any[]) {
+    this.log("info", tag, message, ...args);
+  }
+
+  warn(tag: string, message: string, ...args: any[]) {
+    this.log("warn", tag, message, ...args);
+  }
+
+  error(tag: string, message: string, ...args: any[]) {
+    this.log("error", tag, message, ...args);
+  }
+
+  setLogLevel(level: LogLevel) {
+    this.level = level;
+  }
+}
+
+function getLogConfigFromParams(): { enable: boolean; level: LogLevel } {
   if (!globalThis || !globalThis.location) {
     return { enable: true, level: "error" };
   }
   const params = new URLSearchParams(globalThis.location.search);
   const enable = !!params.get("debug") || false;
-  const level = params.get("logLevel") as pino.Level | null;
-  const validLevels: pino.Level[] = [
-    "trace",
-    "debug",
-    "info",
-    "warn",
-    "error",
-    "fatal",
-  ];
-  if (level && validLevels.includes(level)) {
+  const level = params.get("logLevel") as LogLevel | null;
+  if (level && LOG_LEVEL_ORDERS.includes(level)) {
     return { enable, level };
   }
-  return { enable, level: "error" };
+  return { enable, level: "debug" };
 }
 
 const LOGGER_CONFIG = getLogConfigFromParams();
-const pinLogger = pino({
-  enabled: LOGGER_CONFIG.enable,
-  browser: {
-    asObject: true,
-  },
+const logger = new BasicLogger({
+  enable: LOGGER_CONFIG.enable,
   level: LOGGER_CONFIG.level,
 });
-
-function formatMessage(tag: string, msg: string) {
-  return `[${tag}]: ${msg}`;
-}
-
-const logger = {
-  trace: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.trace(formatMessage(tag, msg), ...args),
-  debug: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.debug(formatMessage(tag, msg), ...args),
-  info: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.info(formatMessage(tag, msg), ...args),
-  warn: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.warn(formatMessage(tag, msg), ...args),
-  error: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.error(formatMessage(tag, msg), ...args),
-  fatal: (tag: string, msg: string, ...args: any[]) =>
-    pinLogger.fatal(formatMessage(tag, msg), ...args),
-};
 
 export default logger;
