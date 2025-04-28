aaf134a6d1c65a3977a6229dfb7c5babd79f226e
Albdima <alberto.dimauro@mediaset.it>
2025-04-28 17:28:29 +0200

    chore: add temporary polyfill for string for old browser

--- packages/lib/src/polyfills/string.ts
+++ packages/lib/src/polyfills/string.ts
@@ -1,3 +1,8 @@
 if (!String.prototype.trimStart) {
   String.prototype.trimStart = String.prototype.trimLeft;
 }
+if (typeof String.prototype.endsWith !== "function") {
+  String.prototype.endsWith = function (suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
+}
