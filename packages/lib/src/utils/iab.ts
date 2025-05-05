15d1512c00e796848c984cbf826c3525043564a6
Albdima <alberto.dimauro@mediaset.it>
2025-05-05 19:05:17 +0200

    chore: add ConsentCheckOptions to detect iab cmp

--- packages/lib/src/utils/iab.ts
+++ packages/lib/src/utils/iab.ts
@@ -1,14 +1,50 @@
 import { TCString, TCModel, GVL } from "@iabtcf/core";
+import logger from "./logger";
+
+const TAG = "iab";
+
+export interface ConsentCheckOptions {
+  requiredPurposeId?: number;
+  requiredVendorId?: number;
+  checkLegitimateInterest?: boolean;
+}
 
 /**
  * Function to retrive consent data from tcString
  * @param tcString
  * @returns
  */
-export function getConsentData(tcString: string): Promise<TCModel> {
-  const tcModel = TCString.decode(tcString);
-  const gvl = new GVL();
-  return gvl.readyPromise.then(() => tcModel);
+export async function getConsentData(
+  tcString: string,
+  options?: ConsentCheckOptions
+): Promise<{
+  tcModel: TCModel;
+  hasPurposeConsent: boolean;
+  hasVendorConsent: boolean;
+}> {
+  try {
+    const gvl = new GVL();
+    await gvl.readyPromise;
+    const tcModel = TCString.decode(tcString);
+    const { requiredPurposeId, requiredVendorId } = options || {};
+
+    const hasPurposeConsent = requiredPurposeId
+      ? !!tcModel.purposeConsents?.has(requiredPurposeId)
+      : true;
+
+    const hasVendorConsent = requiredVendorId
+      ? !!tcModel.vendorConsents?.has(requiredVendorId)
+      : true;
+
+    return {
+      tcModel,
+      hasPurposeConsent,
+      hasVendorConsent,
+    };
+  } catch (error) {
+    logger.error(TAG, "Failed to decode TC string:", error);
+    throw new Error("Could not decode consent data");
+  }
 }
 
 export function getTCData(): Promise<{
