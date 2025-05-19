9af343a74856cccdf9d84a55cf26b7778022fc68
Albdima <alberto.dimauro@mediaset.it>
2025-05-19 14:29:33 +0200

    chore: export debug from ima.js

--- packages/lib/src/ima3.ts
+++ packages/lib/src/ima3.ts
@@ -3,4 +3,5 @@ import { version } from "../package.json";
 export const VERSION = version;
 
 export * from "./custom/ima/index";
-export * from "./types/index";
+export * from "./custom/ima/index";
+export * from "./debug";
