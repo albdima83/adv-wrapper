20fee1cdc32cefefdde1244ceb7b2b69c657da56
Albdima <alberto.dimauro@mediaset.it>
2025-05-27 00:19:06 +0200

    chore: add meta data to index demo

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,14 +1,54 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { version } from "./package.json";
 
-export default defineConfig({
-	base: process.env.BASE_URL || "/",
-	build: {
-		outDir: "dist",
-		rollupOptions: {
-			input: "index.html",
+export default defineConfig((config: ConfigEnv) => {
+	const mode = config.mode || "development";
+	const define = loadEnv(mode, process.cwd(), "");
+	return {
+		base: define.BASE_URL || "/",
+		build: {
+			outDir: "dist",
+			rollupOptions: {
+				input: "index.html",
+				output: {
+					entryFileNames: "assets/js/[name]-[hash].js",
+					chunkFileNames: "assets/js/[name]-[hash].js",
+					assetFileNames: "assets/[ext]/[name]-[hash][extname]",
+				},
+			},
 		},
-	},
-	define: {
-		__IMA_LIB_URL__: JSON.stringify(process.env.IMA_LIB_URL || ""),
-	},
+		define: {
+			__IMA_LIB_URL__: JSON.stringify(define.IMA_LIB_URL || ""),
+		},
+		plugins: [
+			ViteEjsPlugin(
+				{
+					meta: [
+						{
+							name: "enviroment",
+							value: `${mode}`,
+						},
+						{
+							name: "version",
+							value: `${version}`,
+						},
+						{
+							name: "imaLib",
+							value: `${define.IMA_LIB_URL}`,
+						},
+						{
+							name: "baseUrl",
+							value: `${define.BASE_URL}`,
+						},
+					],
+				},
+				{
+					ejs: {
+						beautify: true,
+					},
+				},
+			),
+		],
+	};
 });
